5cd76cde82208fc0213af37fcd4410b360be205e
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2022-12-28 22:59:22 +0000

    Google Maps added

--- kontakt.php
+++ kontakt.php
@@ -32,3 +32,5 @@
     </div>
 </div>
 <br>
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1502.8915313618138!2d21.72402036308127!3d50.691136530182305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4722b4ff1e63c4f7%3A0x1c5a439cd1a66685!2sO%C5%BCarowska%201%2C%2027-600%20Sandomierz%2C%20Polska!5e0!3m2!1spl!2suk!4v1672268210859!5m2!1spl!2suk" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
