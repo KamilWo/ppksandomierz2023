7ae38d778dddaec0ea539b10fd01ccac24face06
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2023-07-30 15:23:44 +0100

    2023-07-30

--- archiwum12.php
+++ archiwum12.php
@@ -19,7 +19,214 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2023 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 23.&nbsp;lipca 2023&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 30.&nbsp;lipca 2023&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    24.07. – 30.07.2023&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td colspan="2" class="center"><b>24&nbsp;lipca, Poniedziałek</b></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">6.30</td>
+                                    <td>+ Franciszka w&nbsp;rocz.&nbsp;śm. i&nbsp;Aleksandrę,
+                                    Kazimierza i&nbsp;Barbarę Burek</td>
+                                </tr>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">18.00</td>
+                                    <td>+ Mariannę Dubiel</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td colspan="2" class="center"><b>25&nbsp;lipca, Wtorek</b></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">6.30</td>
+                                    <td>+ Mieczysława Świerkulę</td>
+                                </tr>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">18.00</td>
+                                    <td>+ Krzysztofa, Józefa, Stefanię Łukawskich, Stanisława
+                                    i&nbsp;Leokadię Majcher</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td colspan="2" class="center"><b>26&nbsp;lipca, Środa</b></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">6.30</td>
+                                    <td>Dzięk-błagalna o&nbsp;Boże Błogosławieństwo dla
+                                    Dawida</td>
+                                </tr>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">18.00</td>
+                                    <td>+ Józefę i&nbsp;Stanisława Wolaków, Mariannę
+                                    i&nbsp;Adama Bąków, i&nbsp;zm. z&nbsp;rodz.</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td colspan="2" class="center"><b>27&nbsp;lipca, Czwartek</b></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">6.30</td>
+                                    <td>W&nbsp;int. Anny z&nbsp;racji imienin – o&nbsp;Boże
+                                    błogosławieństwo, opiekę Matki Bożej</td>
+                                </tr>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">18.00</td>
+                                    <td>O&nbsp;Boże Błog., dary Ducha&nbsp;Św., opiekę Matki
+                                    Bożej i&nbsp;św.&nbsp;Józefa, i&nbsp;o&nbsp;dobrą żonę
+                                dla Mateusza</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td colspan="2" class="center"><b>28&nbsp;lipca, Piątek</b></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">6.30</td>
+                                    <td>+ Stefanię Seweryńską w&nbsp;rocz.&nbsp;śm.</td>
+                                </tr>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">18.00</td>
+                                    <td>+ Mieczysława Świerkulę</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td colspan="2" class="center"><b>29&nbsp;lipca, Sobota</b></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">6.30</td>
+                                    <td>+ Zbigniewa Dębickiego</td>
+                                </tr>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">18.00</td>
+                                    <td>+ Zbigniewa Dębickiego</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td colspan="2" class="center"><b>30&nbsp;lipca, Niedziela</b></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">7.30</td>
+                                    <td>+ Teodorę i&nbsp;Stanisława Judów</td>
+                                </tr>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">9.30</td>
+                                    <td>+ Mariannę 16&nbsp;rocz.&nbsp;śm., Romana Patyna,
+                                    Włodzimierza Bartosika, Mariannę, Joachima, Wandę,
+                                    Władysława Bartosików</td>
+                                </tr>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">11.15</td>
+                                    <td>+ Ryszarda Mizielskiego w&nbsp;rocz.&nbsp;śm.,
+                                    Agnieszkę, Bartłomieja, Andrzeja, Krystynę, Tadeusza
+                                    Mizielskich</td>
+                                </tr>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">12.30</td>
+                                    <td>+ Józefa i&nbsp;Karolinę Matesa</td>
+                                </tr>
+                                <tr>
+                                    <td>&nbsp;</td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td class="center">18.00</td>
+                                    <td>+ Stefana w&nbsp;rocz.&nbsp;śm., Genowefę, Jarosława,
+                                    Annę, Józefa Burków, Paulinę, Mieczysława Ramusów, Janinę,
+                                    Stefana Plachów, Kazimierę Pietras, Marię Potocką, Józefę
+                                    i&nbsp;Romana Siudak</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>23.&nbsp;lipca 2023&nbsp;r.</h3>
+                                <h3>XVI&nbsp;Niedziela Zwykła, Rok&nbsp;A</h3>
+                            </div>
+                            <div class="col">
+                                <p>1. We&nbsp;wtorek przypada Dzień imienin księdza Biskupa
+                                Krzysztofa Nitkiewicza. Prosimy o&nbsp;dar modlitwy w&nbsp;intencji
+                                Czcigodnego Solenizanta.</p>
+                                <p>2. Przypominamy, że&nbsp;Wyższe Seminarium Duchowne
+                                w&nbsp;Sandomierzu przyjmuje zgłoszenia nowych kandydatów, którzy
+                                chcieliby rozpocząć formację i&nbsp;studia przygotowujące
+                                do&nbsp;sakramentu kapłaństwa. Chętni mogą zgłaszać się osobiście
+                                w&nbsp;siedzibie Seminarium Duchownego w&nbsp;Sandomierzu,
+                                ul.&nbsp;Żeromskiego&nbsp;6. Możliwy jest także kontakt telefoniczny
+                                (tel.:&nbsp;511&nbsp;413&nbsp;002) lub mailowy
+                                (rozeznajpow@gmail.com).</p>
+                                <p>3. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+                                Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.
+                                Są&nbsp;czasopisma „Niedziela”, „Miłujcie się”, „Gość Niedzielny”
+                                i&nbsp;„Mały Gość Niedzielny”.</p>
+                                <p>4. W&nbsp;dniach od&nbsp;4 do&nbsp;12&nbsp;sierpnia&nbsp;br.
+                                odbędzie się XL&nbsp;Piesza Pielgrzymka Ziemi Sandomierskiej
+                                na&nbsp;Jasną Górę pod hasłem „Matko Kościoła prowadź. Więcej
+                                informacji na&nbsp;plakacie w&nbsp;gablocie przy głównym wejściu
+                                do&nbsp;kościoła. Zapraszamy na&nbsp;pątniczy szlak.</p>
+                                <p>5. Duszpasterstwo Służby Liturgicznej Diecezji Sandomierskiej
+                                zaprasza na&nbsp;Wakacyjną Szkołę Lektora połączoną
+                                z&nbsp;rekolekcjami. Propozycja skierowana jest do&nbsp;ministrantów,
+                                którzy ukończyli VII&nbsp;klasę i&nbsp;pragną posługiwać czytając
+                                Słowo Boże w&nbsp;czasie celebracji liturgicznych. Zapisy
+                                i&nbsp;szczegółowe informacje u&nbsp;księży wikariuszy.</p>
+                                <p>6. W&nbsp;niedzielę 6&nbsp;sierpnia w&nbsp;Zawichoście odbędą się
+                                Dni Młodych naszej Diecezji w&nbsp;łączności z&nbsp;Ojcem Świętym
+                                Franciszkiem, który w&nbsp;tym czasie będzie z&nbsp;młodymi
+                                z&nbsp;całego świata w&nbsp;Lizbonie. Zapraszamy młodzież
+                                do&nbsp;uczestnictwa w&nbsp;tym wydarzeniu. Szczegółowe informacje
+                                i&nbsp;zapisy u&nbsp;Księży wikariuszy.</p>
+                                <p>7. Dziękujemy parafianom za&nbsp;modlitwę, wszelką pomoc, składane
+                                ofiary na&nbsp;potrzeby parafii. Wszystkim życzymy błogosławionej
+                                Niedzieli.</p>
+                                <p>
+                                    <span style="float: left;">Sandomierz, 23.&nbsp;lipca 2023&nbsp;r.</span>
+                                    <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
+                                </p>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,19 +1,18 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            24.07. – 30.07.2023&nbsp;r.
+            31.07. – 06.08.2023&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td colspan="2" class="center"><b>24&nbsp;lipca, Poniedziałek</b></td>
+            <td colspan="2" class="center"><b>31&nbsp;lipca, Poniedziałek</b></td>
         </tr>
         <tr>
             <td class="center">6.30</td>
-            <td>+ Franciszka w&nbsp;rocz.&nbsp;śm. i&nbsp;Aleksandrę,
-            Kazimierza i&nbsp;Barbarę Burek</td>
+            <td>+ Izabelę Sochę</td>
         </tr>
         <tr>
             <td>&nbsp;</td>
@@ -21,76 +20,10 @@
         </tr>
         <tr>
             <td class="center">18.00</td>
-            <td>+ Mariannę Dubiel</td>
+            <td>+ Małgorzatę Walczynę – 6&nbsp;rocz.&nbsp;śm.</td>
         </tr>
         <tr class="actualnosci_day">
-            <td colspan="2" class="center"><b>25&nbsp;lipca, Wtorek</b></td>
-        </tr>
-        <tr>
-            <td class="center">6.30</td>
-            <td>+ Mieczysława Świerkulę</td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td class="center">18.00</td>
-            <td>+ Krzysztofa, Józefa, Stefanię Łukawskich, Stanisława
-            i&nbsp;Leokadię Majcher</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td colspan="2" class="center"><b>26&nbsp;lipca, Środa</b></td>
-        </tr>
-        <tr>
-            <td class="center">6.30</td>
-            <td>Dzięk-błagalna o&nbsp;Boże Błogosławieństwo dla
-            Dawida</td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td class="center">18.00</td>
-            <td>+ Józefę i&nbsp;Stanisława Wolaków, Mariannę
-            i&nbsp;Adama Bąków, i&nbsp;zm. z&nbsp;rodz.</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td colspan="2" class="center"><b>27&nbsp;lipca, Czwartek</b></td>
-        </tr>
-        <tr>
-            <td class="center">6.30</td>
-            <td>W&nbsp;int. Anny z&nbsp;racji imienin – o&nbsp;Boże
-            błogosławieństwo, opiekę Matki Bożej</td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td class="center">18.00</td>
-            <td>O&nbsp;Boże Błog., dary Ducha&nbsp;Św., opiekę Matki
-            Bożej i&nbsp;św.&nbsp;Józefa, i&nbsp;o&nbsp;dobrą żonę
-        dla Mateusza</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td colspan="2" class="center"><b>28&nbsp;lipca, Piątek</b></td>
-        </tr>
-        <tr>
-            <td class="center">6.30</td>
-            <td>+ Stefanię Seweryńską w&nbsp;rocz.&nbsp;śm.</td>
-        </tr>
-        <tr>
-            <td>&nbsp;</td>
-            <td></td>
-        </tr>
-        <tr>
-            <td class="center">18.00</td>
-            <td>+ Mieczysława Świerkulę</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td colspan="2" class="center"><b>29&nbsp;lipca, Sobota</b></td>
+            <td colspan="2" class="center"><b>1&nbsp;sierpnia, Wtorek</b></td>
         </tr>
         <tr>
             <td class="center">6.30</td>
@@ -102,14 +35,82 @@
         </tr>
         <tr>
             <td class="center">18.00</td>
-            <td>+ Zbigniewa Dębickiego</td>
+            <td>+ Mariannę i&nbsp;Eugeniusza Jedlińskich</td>
         </tr>
         <tr class="actualnosci_day">
-            <td colspan="2" class="center"><b>30&nbsp;lipca, Niedziela</b></td>
+            <td colspan="2" class="center"><b>2&nbsp;sierpnia, Środa</b></td>
+        </tr>
+        <tr>
+            <td class="center">6.30</td>
+            <td>+ Zbigniewa Dębickiego</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td class="center">18.00</td>
+            <td>+ Stanisławę, Jana, Stanisława, Witolda, Mariana,
+            Krystynę, Mariana</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td colspan="2" class="center"><b>3&nbsp;sierpnia, Czwartek</b></td>
+        </tr>
+        <tr>
+            <td class="center">6.30</td>
+            <td>+ Henryka Kwietnia i&nbsp;zm. z&nbsp;rodz.</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td class="center">18.00</td>
+            <td>+ Stanisławę i&nbsp;Stanisława Wolińskich</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td colspan="2" class="center"><b>4&nbsp;sierpnia, Piątek</b></td>
+        </tr>
+        <tr>
+            <td class="center">6.30</td>
+            <td>+ Marię Włoch 3&nbsp;rocz.&nbsp;śm., Wincentego
+            Kołodzieja, Juliannę, Walentego Bulirów, Jerzego,
+            Zdzisława, Michała, Ewę, Marię, Józefa, Kazimierza
+            z&nbsp;rodz. Włochów, Stanisławę i&nbsp;Stanisława
+            Wosiów</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td class="center">18.00</td>
+            <td>W&nbsp;int. wynagradzającej Panu Bogu za&nbsp;grzechy
+            aborcji i&nbsp;eutanazji</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td colspan="2" class="center"><b>5&nbsp;sierpnia, Sobota</b></td>
+        </tr>
+        <tr>
+            <td class="center">6.30</td>
+            <td>+ Wiesławę Wałcerz 16&nbsp;rocz.&nbsp;śm., Stefanię,
+            Franciszka Kowalskich, Marię Woś</td>
+        </tr>
+        <tr>
+            <td>&nbsp;</td>
+            <td></td>
+        </tr>
+        <tr>
+            <td class="center">18.00</td>
+            <td>W&nbsp;int. wynagradzającej NSM za&nbsp;grzechy
+            świata</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td colspan="2" class="center"><b>6&nbsp;sierpnia, Niedziela</b></td>
         </tr>
         <tr>
             <td class="center">7.30</td>
-            <td>+ Teodorę i&nbsp;Stanisława Judów</td>
+            <td>Za&nbsp;parafian</td>
         </tr>
         <tr>
             <td>&nbsp;</td>
@@ -117,9 +118,7 @@
         </tr>
         <tr>
             <td class="center">9.30</td>
-            <td>+ Mariannę 16&nbsp;rocz.&nbsp;śm., Romana Patyna,
-            Włodzimierza Bartosika, Mariannę, Joachima, Wandę,
-            Władysława Bartosików</td>
+            <td>+ Katarzynę i&nbsp;Franciszka Sadowskich</td>
         </tr>
         <tr>
             <td>&nbsp;</td>
@@ -127,9 +126,7 @@
         </tr>
         <tr>
             <td class="center">11.15</td>
-            <td>+ Ryszarda Mizielskiego w&nbsp;rocz.&nbsp;śm.,
-            Agnieszkę, Bartłomieja, Andrzeja, Krystynę, Tadeusza
-            Mizielskich</td>
+            <td>KŻR</td>
         </tr>
         <tr>
             <td>&nbsp;</td>
@@ -137,7 +134,9 @@
         </tr>
         <tr>
             <td class="center">12.30</td>
-            <td>+ Józefa i&nbsp;Karolinę Matesa</td>
+            <td>Dzięk.-błag. z&nbsp;racji 25&nbsp;rocz.
+            ur.&nbsp;Andrzeja o&nbsp;B.&nbsp;Bł., potrzebne łaski
+            i&nbsp;opiekę MB.</td>
         </tr>
         <tr>
             <td>&nbsp;</td>
@@ -145,10 +144,7 @@
         </tr>
         <tr>
             <td class="center">18.00</td>
-            <td>+ Stefana w&nbsp;rocz.&nbsp;śm., Genowefę, Jarosława,
-            Annę, Józefa Burków, Paulinę, Mieczysława Ramusów, Janinę,
-            Stefana Plachów, Kazimierę Pietras, Marię Potocką, Józefę
-            i&nbsp;Romana Siudak</td>
+            <td>+ Julię, Antoniego Serafinów</td>
         </tr>
     </tbody>
 </table>
