384f2f5804370f8777404b10c606b5a5351bfc05
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2023-10-16 05:27:57 +0100

    2023-10-15

--- archiwum12.php
+++ archiwum12.php
@@ -19,7 +19,174 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2023 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 8.&nbsp;października 2023&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 15.&nbsp;października 2023&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    9.10. – 15.10.2023&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 9&nbsp;października</b></td>
+                                    <td><b>Uroczystość bł.&nbsp;Wincentego Kadłubka</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Wojciecha Dobrocha</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Tadeusza rocz.&nbsp;śm., Irenę, Krzysztofa
+                                    i&nbsp;Mirosława Kołaczów</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Mariannę, Józefa Zuchowiczów, Mirka Wilczewskiego</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 10&nbsp;października</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Stefanię 16&nbsp;rocz.&nbsp;śm., ranciszka
+                                    Kowalskich, Marię Woś, Wiesławę Wałcerz</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>W&nbsp;int. osób uzależnionych i&nbsp;ich rodzin
+                                    o&nbsp;łaskę uzdrowienia, i&nbsp;wyzwolenie</td></tr>
+                                    <tr style="border-bottom: 2px solid black;">
+                                    <td>O&nbsp;nawrócenie męża Piotra i&nbsp;ojca
+                                    Tadeusza</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 11&nbsp;października</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Alicję Machulę</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Bogusława Tetera</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Krystynę Dyl</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 12&nbsp;października</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Alicję Machulę</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Aleksandra Krakowiaka 10&nbsp;rocz.&nbsp;śm.,
+                                    i&nbsp;zm. z&nbsp;rodz.</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Helenę Król, zm.&nbsp;z&nbsp;rodz. Królów,
+                                    Ostrowskich, Szczębarów, Golców, Bednarskich, dusze
+                                    w&nbsp;czyśćcu cierpiące</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 13&nbsp;października</b></td>
+                                    <td><b>Wspomnienie bł.&nbsp;Honorata Koźmińskiego</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Kazimierza Płazę, zm.&nbsp;z&nbsp;rodz. i&nbsp;dusze
+                                    w&nbsp;czyśćcu cierpiące</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Marię, Władysława, Czesława Barańskich, Andrzeja
+                                    Krawczyka</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Barbarę Byra</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 14&nbsp;października</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Bogusława Tetera</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Juliannę Foryś, Antoniego, Jana, Zenona Kędziorów,
+                                    Teresę, Grażynę Kędziorów, zm.&nbsp;z&nbsp;rodz. Kędziorów
+                                    i&nbsp;Maruszczaków, Annę Ziomek</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Macieja Wodza, Bożenę Kubala 1&nbsp;rocz.&nbsp;śm.,
+                                    Joannę, Florentynę, Mieczysława Sado, Stefana, Krystynę
+                                    Wódz</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 15&nbsp;października</b></td>
+                                    <td><b>
+                                        <span style="color: red">XXVIII Niedziela Zwykła</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Alicję Gilewską, Józefa, Stefanię Janus</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>O&nbsp;Boże błog., dla Jakuba Sałkowskiego w&nbsp;18
+                                    rocz.&nbsp;ur.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>DK</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Barbarę Pawlikowską 5&nbsp;rocz.&nbsp;śm.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_intentions.php
+++ last_intentions.php
@@ -1,59 +1,41 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZY ŚWIĘTYCH<br>
-            9.10. – 15.10.2023&nbsp;r.
+            16.10. – 22.10.2023&nbsp;r.
         </h3>
     </div>
     <div class="col">
 <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
     <tbody>
         <tr class="actualnosci_day">
-            <td><b>PONIEDZIAŁEK, 9&nbsp;października</b></td>
-            <td><b>Uroczystość bł.&nbsp;Wincentego Kadłubka</b></td>
+            <td><b>PONIEDZIAŁEK, 16&nbsp;października</b></td>
+            <td><b>Wspomnienie św.&nbsp;Jadwigi Śląskiej</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
+            <td>+ Marię Stachowicz</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">17.30</td>
+            <td><strong>Nabożeństwo Różańcowe</strong></td>
+        </tr>
+        <tr>
+            <td class="center" rowspan="2">18.00</td>
+            <td>+ Wiktora Krysę 15&nbsp;rocz.&nbsp;śm., Józefę
+            i&nbsp;Jana Krysów, Wojciecha Krysa</td>
+        </tr><tr style="border-bottom: 2px solid black;">
             <td>+ Wojciecha Dobrocha</td>
         </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">17.30</td>
-            <td><strong>Nabożeństwo Różańcowe</strong></td>
-        </tr>
-        <tr>
-            <td class="center" rowspan="2">18.00</td>
-            <td>+ Tadeusza rocz.&nbsp;śm., Irenę, Krzysztofa
-            i&nbsp;Mirosława Kołaczów</td>
-        </tr><tr style="border-bottom: 2px solid black;">
-            <td>+ Mariannę, Józefa Zuchowiczów, Mirka Wilczewskiego</td>
-        </tr>
         <tr class="actualnosci_day">
-            <td><b>WTOREK, 10&nbsp;października</b></td>
-            <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Stefanię 16&nbsp;rocz.&nbsp;śm., ranciszka
-            Kowalskich, Marię Woś, Wiesławę Wałcerz</td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">17.30</td>
-            <td><strong>Nabożeństwo Różańcowe</strong></td>
+            <td><b>WTOREK, 17&nbsp;października</b></td>
+            <td><b>Wspomnienie św.&nbsp;Ignacego Antiocheńskiego,
+            Bp. i&nbsp;Męczennika</b></td>
         </tr>
         <tr>
-            <td class="center" rowspan="2">18.00</td>
-            <td>W&nbsp;int. osób uzależnionych i&nbsp;ich rodzin
-            o&nbsp;łaskę uzdrowienia, i&nbsp;wyzwolenie</td></tr>
+            <td class="center" rowspan="2">6.30</td>
+            <td>+ Krystynę Mikula</td></tr>
             <tr style="border-bottom: 2px solid black;">
-            <td>O&nbsp;nawrócenie męża Piotra i&nbsp;ojca
-            Tadeusza</td>
-        </tr>
-        <tr class="actualnosci_day">
-            <td><b>ŚRODA, 11&nbsp;października</b></td>
-            <td><b></b></td>
-        </tr>
-        <tr style="border-bottom: 2px solid black;">
-            <td class="center">6.30</td>
-            <td>+ Alicję Machulę</td>
+            <td>+ Lidię Gielerak</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">17.30</td>
@@ -61,17 +43,36 @@
         </tr>
         <tr>
             <td class="center" rowspan="2">18.00</td>
+            <td>+ Wojciecha Dobrocha</td></tr>
+            <tr style="border-bottom: 2px solid black;">
             <td>+ Bogusława Tetera</td>
-        </tr><tr style="border-bottom: 2px solid black;">
-            <td>+ Krystynę Dyl</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>CZWARTEK, 12&nbsp;października</b></td>
+            <td><b>ŚRODA, 18&nbsp;października</b></td>
+            <td><b>Świeto Św.&nbsp;Łukasza Ewangelisty</b></td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">6.30</td>
+            <td>+ Bogusława Tetera</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">17.30</td>
+            <td><strong>Nabożeństwo Różańcowe</strong></td>
+        </tr>
+        <tr>
+            <td class="center" rowspan="2">18.00</td>
+            <td>+ Józefę i&nbsp;Stanisława Zelików</td>
+        </tr><tr style="border-bottom: 2px solid black;">
+            <td>+ Anielę i&nbsp;Franciszka Sieciarskich, Jana
+            Rutkowskiego</td>
+        </tr>
+        <tr class="actualnosci_day">
+            <td><b>CZWARTEK, 19&nbsp;października</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Alicję Machulę</td>
+            <td>+ Pawła Guza</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">17.30</td>
@@ -79,21 +80,18 @@
         </tr>
         <tr>
             <td class="center" rowspan="2">18.00</td>
-            <td>+ Aleksandra Krakowiaka 10&nbsp;rocz.&nbsp;śm.,
-            i&nbsp;zm. z&nbsp;rodz.</td>
+            <td>+ Stanisława Kabzę</td>
         </tr><tr style="border-bottom: 2px solid black;">
-            <td>+ Helenę Król, zm.&nbsp;z&nbsp;rodz. Królów,
-            Ostrowskich, Szczębarów, Golców, Bednarskich, dusze
-            w&nbsp;czyśćcu cierpiące</td>
+            <td>+ Andrzeja Popka</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>PIĄTEK, 13&nbsp;października</b></td>
-            <td><b>Wspomnienie bł.&nbsp;Honorata Koźmińskiego</b></td>
+            <td><b>PIĄTEK, 20&nbsp;października</b></td>
+            <td><b>Wspomnienie Św.&nbsp;Jana Kantego,
+            prezbitera</b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Kazimierza Płazę, zm.&nbsp;z&nbsp;rodz. i&nbsp;dusze
-            w&nbsp;czyśćcu cierpiące</td>
+            <td>+ Władysławę i&nbsp;Czesława Buczków</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">17.30</td>
@@ -101,18 +99,21 @@
         </tr>
         <tr>
             <td class="center" rowspan="2">18.00</td>
-            <td>+ Marię, Władysława, Czesława Barańskich, Andrzeja
-            Krawczyka</td>
+            <td>+ Mariana Małysa 14&nbsp;rocz.&nbsp;śm.</td>
         </tr><tr style="border-bottom: 2px solid black;">
             <td>+ Barbarę Byra</td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>SOBOTA, 14&nbsp;października</b></td>
+            <td><b>SOBOTA, 21&nbsp;października</b></td>
             <td><b></b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">6.30</td>
-            <td>+ Bogusława Tetera</td>
+            <td>Bogusława Tetera</td>
+        </tr>
+        <tr style="border-bottom: 2px solid black;">
+            <td class="center">13.00</td>
+            <td>Dzień Wspólnoty Domowego Kościoła</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">17.30</td>
@@ -120,36 +121,34 @@
         </tr>
         <tr>
             <td class="center" rowspan="2">18.00</td>
-            <td>+ Juliannę Foryś, Antoniego, Jana, Zenona Kędziorów,
-            Teresę, Grażynę Kędziorów, zm.&nbsp;z&nbsp;rodz. Kędziorów
-            i&nbsp;Maruszczaków, Annę Ziomek</td>
+            <td>+ Bronisławę Pęk 6&nbsp;rocz.&nbsp;śm., Piotra,
+            Katarzynę, Stanisława Pęk, Antoniego i&nbsp;Stefanię
+            Dobek</td>
         </tr><tr style="border-bottom: 2px solid black;">
-            <td>+ Macieja Wodza, Bożenę Kubala 1&nbsp;rocz.&nbsp;śm.,
-            Joannę, Florentynę, Mieczysława Sado, Stefana, Krystynę
-            Wódz</td>
+            <td>+ Jerzego Surgała 6&nbsp;rocz.&nbsp;śm. </td>
         </tr>
         <tr class="actualnosci_day">
-            <td><b>NIEDZIELA, 15&nbsp;października</b></td>
+            <td><b>NIEDZIELA, 22&nbsp;października</b></td>
             <td><b>
-                <span style="color: red">XXVIII Niedziela Zwykła</span>
+                <span style="color: red">XXIX Niedziela Zwykła</span>
             </b></td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">7.30</td>
-            <td>+ Alicję Gilewską, Józefa, Stefanię Janus</td>
+            <td>+ Annę Podsiadły</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">9.30</td>
-            <td>O&nbsp;Boże błog., dla Jakuba Sałkowskiego w&nbsp;18
-            rocz.&nbsp;ur.</td>
+            <td>+ Franciszka Zająca</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">11.15</td>
-            <td>Za&nbsp;parafian</td>
+            <td>+ Janinę i&nbsp;Józefa Tutak</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">12.30</td>
-            <td>DK</td>
+            <td>+ Dorotę Kwitek 20&nbsp;rocz.&nbsp;śm., Edwarda
+            Stefańskiego 4&nbsp;rocz.&nbsp;śm.</td>
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">17.30</td>
@@ -157,7 +156,7 @@
         </tr>
         <tr style="border-bottom: 2px solid black;">
             <td class="center">18.00</td>
-            <td>+ Barbarę Pawlikowską 5&nbsp;rocz.&nbsp;śm.</td>
+            <td>+ Za&nbsp;parafian</td>
         </tr>
     </tbody>
 </table>
