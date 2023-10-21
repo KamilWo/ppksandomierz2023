fa5515a5edc9490450a94f7f5271442c3d2478e5
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2023-10-21 21:58:13 +0100

    2023-10-15

--- archiwum12.php
+++ archiwum12.php
@@ -19,7 +19,216 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2023 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 15.&nbsp;października 2023&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 22.&nbsp;października 2023&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    16.10. – 22.10.2023&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 16&nbsp;października</b></td>
+                                    <td><b>Wspomnienie św.&nbsp;Jadwigi Śląskiej</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Marię Stachowicz</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Wiktora Krysę 15&nbsp;rocz.&nbsp;śm., Józefę
+                                    i&nbsp;Jana Krysów, Wojciecha Krysa</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Wojciecha Dobrocha</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 17&nbsp;października</b></td>
+                                    <td><b>Wspomnienie św.&nbsp;Ignacego Antiocheńskiego,
+                                    Bp. i&nbsp;Męczennika</b></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">6.30</td>
+                                    <td>+ Krystynę Mikula</td></tr>
+                                    <tr style="border-bottom: 2px solid black;">
+                                    <td>+ Lidię Gielerak</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Wojciecha Dobrocha</td></tr>
+                                    <tr style="border-bottom: 2px solid black;">
+                                    <td>+ Bogusława Tetera</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 18&nbsp;października</b></td>
+                                    <td><b>Świeto Św.&nbsp;Łukasza Ewangelisty</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Bogusława Tetera</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Józefę i&nbsp;Stanisława Zelików</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Anielę i&nbsp;Franciszka Sieciarskich, Jana
+                                    Rutkowskiego</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 19&nbsp;października</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Pawła Guza</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Stanisława Kabzę</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Andrzeja Popka</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 20&nbsp;października</b></td>
+                                    <td><b>Wspomnienie Św.&nbsp;Jana Kantego,
+                                    prezbitera</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Władysławę i&nbsp;Czesława Buczków</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Mariana Małysa 14&nbsp;rocz.&nbsp;śm.</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Barbarę Byra</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 21&nbsp;października</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>Bogusława Tetera</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">13.00</td>
+                                    <td>Dzień Wspólnoty Domowego Kościoła</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Bronisławę Pęk 6&nbsp;rocz.&nbsp;śm., Piotra,
+                                    Katarzynę, Stanisława Pęk, Antoniego i&nbsp;Stefanię
+                                    Dobek</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Jerzego Surgała 6&nbsp;rocz.&nbsp;śm. </td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 22&nbsp;października</b></td>
+                                    <td><b>
+                                        <span style="color: red">XXIX Niedziela Zwykła</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>+ Annę Podsiadły</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Franciszka Zająca</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>+ Janinę i&nbsp;Józefa Tutak</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>+ Dorotę Kwitek 20&nbsp;rocz.&nbsp;śm., Edwarda
+                                    Stefańskiego 4&nbsp;rocz.&nbsp;śm.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>+ Za&nbsp;parafian</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>15.&nbsp;października 2023&nbsp;r.</h3>
+                                <h3>XXVIII&nbsp;Niedziela Zwykła, Rok&nbsp;A</h3>
+                            </div>
+                            <div class="col">
+                                <p>1. Dziś po&nbsp;Mszach&nbsp;św. zbiórka do&nbsp;puszek
+                                na&nbsp;wsparcie Dzieła Nowego Tysiąclecia – stypendia dla zdolnej
+                                a&nbsp;ubogiej młodzieży.</p>
+                                <p>2. Kartki na&nbsp;wypominki na&nbsp;stoliku obok gazet.</p>
+                                <p>3. W&nbsp;najbliższy wtorek, jak w&nbsp;każdy trzeci wtorek
+                                miesiąca w&nbsp;tym roku, zapraszamy na&nbsp;kolejne spotkanie
+                                Akademii Biblijnej w&nbsp;Sandomierzu; poprowadzi je&nbsp;wykładowca
+                                Pisma świętego w&nbsp;naszym seminarium, ks.&nbsp;Paweł Lasek.
+                                Tematem tego spotkania będzie: Zdobycie Jerycha i&nbsp;Kanaanu przez
+                                Jozuego. Spotkanie będzie we&nbsp;wtorek, 17&nbsp;października
+                                o&nbsp;godz.&nbsp;18.45, w&nbsp;auli Wyższego Seminarium Duchownego.
+                                Serdecznie zapraszamy!</p>
+                                <p>4. Zapraszamy na&nbsp;wykład otwarty z&nbsp;filozofii wraz
+                                prezentacja pt. „Sztuczna Inteligencja szansa czy zagrożenie?” wykład
+                                odbędzie się w&nbsp;sobotę 21&nbsp;października br.
+                                o&nbsp;godz.&nbsp;16.00 w&nbsp;domu bł.&nbsp;Antoniego Rewery przy
+                                kościele św.&nbsp;Józefa w&nbsp;Sandomierzu. Szczegóły
+                                na&nbsp;plakacie w&nbsp;gablocie.</p>
+                                <p>5. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+                                Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
+                                <p>6. W&nbsp;przyszłą niedzielę liczenie wiernych biorących udział
+                                we&nbsp;Mszach&nbsp;św. i&nbsp;przyjmujących Komunię&nbsp;Św.</p>
+                                <p>7. Apostolat Margaretka Diecezji Sandomierskiej zaprasza
+                                na&nbsp;Różaniec w&nbsp;intencji kapłanów, który odbędzie się
+                                w&nbsp;najbliższą sobotę o&nbsp;godz.&nbsp;17.30 w&nbsp;naszym
+                                kościele.</p>
+                                <p>8. Dziękujemy parafianom za&nbsp;modlitwę, wszelką pomoc, składane
+                                ofiary na&nbsp;potrzeby parafii. Wszystkim życzymy błogosławionej
+                                Niedzieli.</p>
+                                <p>
+                                    <span style="float: left;">Sandomierz, 15.&nbsp;października 2023&nbsp;r.</span>
+                                    <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
+                                </p>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
@@ -484,6 +693,59 @@ include 'head.php';
                                 </tr>
                             </tbody>
                         </table>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>24.&nbsp;września 2023&nbsp;r.</h3>
+                                <h3>XXV&nbsp;Niedziela Zwykła, Rok&nbsp;A</h3>
+                            </div>
+                            <div class="col">
+                                <p>1. Dnia 8&nbsp;października br. w&nbsp;Sandomierzu odbędzie się
+                                zakończenie Roku Jubileuszowego bł.&nbsp;Wincentego Kadłubka.</p>
+                                <p>2. Pielgrzymka Kół Żywego Różańca do&nbsp;Włostowa, odbędzie się
+                                7&nbsp;października br. Bliższe informacje na&nbsp;plakacie.</p>
+                                <p>3. <strong>W&nbsp;środę</strong> wspomnienie - św.&nbsp;Wincentego
+                                a&nbsp;Paulo, <strong>w czwartek</strong> -  św.&nbsp;Wacława,
+                                <strong>w&nbsp;piątek</strong> - Święto św.&nbsp;Archaniołów Michała,
+                                Gabriela i&nbsp;Rafała, <strong>w&nbsp;sobotę</strong> – wspomnienie
+                                św.&nbsp;Hieronima.</p>
+                                <p>4. W&nbsp;najbliższy wtorek po&nbsp;Mszy Świętej wieczorowej
+                                w&nbsp;kościele spotkanie dla kandydatów do&nbsp;bierzmowania
+                                z&nbsp;klas VI i&nbsp;VII. Prosimy, aby na&nbsp;to&nbsp;spotkanie
+                                młodzież przybyła z&nbsp;rodzicami.</p>
+                                <p>5. W&nbsp;najbliższą sobotę w&nbsp;naszym kościele odbędzie się
+                                pierwszy po&nbsp;przerwie wakacyjnej Wieczór Uwielbienia. Rozpoczęcie
+                                Mszą Świętą o&nbsp;godz.&nbsp;18.00, a&nbsp;po&nbsp;niej Adoracja
+                                Jezusa w&nbsp;Najświętszym Sakramencie. Wszystkich, którzy chcieliby
+                                się zaangażować pod względem muzycznym w&nbsp;to&nbsp;dzieło
+                                zapraszamy na&nbsp;próbę jutro o&nbsp;godz.&nbsp;18.30
+                                do&nbsp;naszego kościoła.</p>
+                                <p>6. W&nbsp;najbliższą niedzielę o&nbsp;17.30 nabożeństwo różańcowe
+                                i&nbsp;tak przez cały miesiąc październik. Każdego dnia dzieci będą
+                                otrzymywały obrazek. Na&nbsp;zakończenie nabożeństwo październikowych
+                                prosimy, aby dzieci przyniosły plansze z&nbsp;obrazkami
+                                do&nbsp;zakrystii.</p>
+                                <p>7. Wszystkie dzieci zachęcamy do&nbsp;uczestnictwa
+                                w&nbsp;parafialnym konkursie plastycznym pt.&nbsp;„Różaniec widziany
+                                oczyma dziecka”. Prace namalowane techniką dowolną należy dostarczyć
+                                do&nbsp;Księży Wikariuszy do&nbsp;22&nbsp;października.
+                                Rozstrzygnięcie konkursu w&nbsp;niedzielę 29&nbsp;października
+                                na&nbsp;Mszy o&nbsp;godz.&nbsp;12.30.</p>
+                                <p>8. W&nbsp;przyszłą niedzielę w&nbsp;kościele, pół godziny przed
+                                Mszą&nbsp;św., która sprawowana jest o&nbsp;godz.&nbsp;11.15 –
+                                adoracja Najśw. Sakramentu połączona z&nbsp;modlitwą różańcową, którą
+                                poprowadzi Koło Żywego Różańca z&nbsp;naszej parafii i&nbsp;zmiana
+                                tajemnic różańcowych. Zapraszamy do&nbsp;wspólnotowej modlitwy.</p>
+                                <p>9. Dziękujemy parafianom za&nbsp;modlitwę, wszelką pomoc, składane
+                                ofiary na&nbsp;potrzeby parafii. Wszystkim życzymy błogosławionej
+                                Niedzieli.</p>
+                                <p>
+                                    <span style="float: left;">Sandomierz, 24.&nbsp;września 2023&nbsp;r.</span>
+                                    <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
+                                </p>
                             </div>
                         </div>
                         <br>
--- last_post.php
+++ last_post.php
@@ -1,50 +1,40 @@
 <div class="card">
     <div class="card-header">
-        <h3>24.&nbsp;września 2023&nbsp;r.</h3>
-        <h3>XXV&nbsp;Niedziela Zwykła, Rok&nbsp;A</h3>
+        <h3>15.&nbsp;października 2023&nbsp;r.</h3>
+        <h3>XXVIII&nbsp;Niedziela Zwykła, Rok&nbsp;A</h3>
     </div>
     <div class="col">
-        <p>1. Dnia 8&nbsp;października br. w&nbsp;Sandomierzu odbędzie się
-        zakończenie Roku Jubileuszowego bł.&nbsp;Wincentego Kadłubka.</p>
-        <p>2. Pielgrzymka Kół Żywego Różańca do&nbsp;Włostowa, odbędzie się
-        7&nbsp;października br. Bliższe informacje na&nbsp;plakacie.</p>
-        <p>3. <strong>W&nbsp;środę</strong> wspomnienie - św.&nbsp;Wincentego
-        a&nbsp;Paulo, <strong>w czwartek</strong> -  św.&nbsp;Wacława,
-        <strong>w&nbsp;piątek</strong> - Święto św.&nbsp;Archaniołów Michała,
-        Gabriela i&nbsp;Rafała, <strong>w&nbsp;sobotę</strong> – wspomnienie
-        św.&nbsp;Hieronima.</p>
-        <p>4. W&nbsp;najbliższy wtorek po&nbsp;Mszy Świętej wieczorowej
-        w&nbsp;kościele spotkanie dla kandydatów do&nbsp;bierzmowania
-        z&nbsp;klas VI i&nbsp;VII. Prosimy, aby na&nbsp;to&nbsp;spotkanie
-        młodzież przybyła z&nbsp;rodzicami.</p>
-        <p>5. W&nbsp;najbliższą sobotę w&nbsp;naszym kościele odbędzie się
-        pierwszy po&nbsp;przerwie wakacyjnej Wieczór Uwielbienia. Rozpoczęcie
-        Mszą Świętą o&nbsp;godz.&nbsp;18.00, a&nbsp;po&nbsp;niej Adoracja
-        Jezusa w&nbsp;Najświętszym Sakramencie. Wszystkich, którzy chcieliby
-        się zaangażować pod względem muzycznym w&nbsp;to&nbsp;dzieło
-        zapraszamy na&nbsp;próbę jutro o&nbsp;godz.&nbsp;18.30
-        do&nbsp;naszego kościoła.</p>
-        <p>6. W&nbsp;najbliższą niedzielę o&nbsp;17.30 nabożeństwo różańcowe
-        i&nbsp;tak przez cały miesiąc październik. Każdego dnia dzieci będą
-        otrzymywały obrazek. Na&nbsp;zakończenie nabożeństwo październikowych
-        prosimy, aby dzieci przyniosły plansze z&nbsp;obrazkami
-        do&nbsp;zakrystii.</p>
-        <p>7. Wszystkie dzieci zachęcamy do&nbsp;uczestnictwa
-        w&nbsp;parafialnym konkursie plastycznym pt.&nbsp;„Różaniec widziany
-        oczyma dziecka”. Prace namalowane techniką dowolną należy dostarczyć
-        do&nbsp;Księży Wikariuszy do&nbsp;22&nbsp;października.
-        Rozstrzygnięcie konkursu w&nbsp;niedzielę 29&nbsp;października
-        na&nbsp;Mszy o&nbsp;godz.&nbsp;12.30.</p>
-        <p>8. W&nbsp;przyszłą niedzielę w&nbsp;kościele, pół godziny przed
-        Mszą&nbsp;św., która sprawowana jest o&nbsp;godz.&nbsp;11.15 –
-        adoracja Najśw. Sakramentu połączona z&nbsp;modlitwą różańcową, którą
-        poprowadzi Koło Żywego Różańca z&nbsp;naszej parafii i&nbsp;zmiana
-        tajemnic różańcowych. Zapraszamy do&nbsp;wspólnotowej modlitwy.</p>
-        <p>9. Dziękujemy parafianom za&nbsp;modlitwę, wszelką pomoc, składane
+        <p>1. Dziś po&nbsp;Mszach&nbsp;św. zbiórka do&nbsp;puszek
+        na&nbsp;wsparcie Dzieła Nowego Tysiąclecia – stypendia dla zdolnej
+        a&nbsp;ubogiej młodzieży.</p>
+        <p>2. Kartki na&nbsp;wypominki na&nbsp;stoliku obok gazet.</p>
+        <p>3. W&nbsp;najbliższy wtorek, jak w&nbsp;każdy trzeci wtorek
+        miesiąca w&nbsp;tym roku, zapraszamy na&nbsp;kolejne spotkanie
+        Akademii Biblijnej w&nbsp;Sandomierzu; poprowadzi je&nbsp;wykładowca
+        Pisma świętego w&nbsp;naszym seminarium, ks.&nbsp;Paweł Lasek.
+        Tematem tego spotkania będzie: Zdobycie Jerycha i&nbsp;Kanaanu przez
+        Jozuego. Spotkanie będzie we&nbsp;wtorek, 17&nbsp;października
+        o&nbsp;godz.&nbsp;18.45, w&nbsp;auli Wyższego Seminarium Duchownego.
+        Serdecznie zapraszamy!</p>
+        <p>4. Zapraszamy na&nbsp;wykład otwarty z&nbsp;filozofii wraz
+        prezentacja pt. „Sztuczna Inteligencja szansa czy zagrożenie?” wykład
+        odbędzie się w&nbsp;sobotę 21&nbsp;października br.
+        o&nbsp;godz.&nbsp;16.00 w&nbsp;domu bł.&nbsp;Antoniego Rewery przy
+        kościele św.&nbsp;Józefa w&nbsp;Sandomierzu. Szczegóły
+        na&nbsp;plakacie w&nbsp;gablocie.</p>
+        <p>5. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+        Zachęcamy do&nbsp;nabywania i&nbsp;czytania prasy katolickiej.</p>
+        <p>6. W&nbsp;przyszłą niedzielę liczenie wiernych biorących udział
+        we&nbsp;Mszach&nbsp;św. i&nbsp;przyjmujących Komunię&nbsp;Św.</p>
+        <p>7. Apostolat Margaretka Diecezji Sandomierskiej zaprasza
+        na&nbsp;Różaniec w&nbsp;intencji kapłanów, który odbędzie się
+        w&nbsp;najbliższą sobotę o&nbsp;godz.&nbsp;17.30 w&nbsp;naszym
+        kościele.</p>
+        <p>8. Dziękujemy parafianom za&nbsp;modlitwę, wszelką pomoc, składane
         ofiary na&nbsp;potrzeby parafii. Wszystkim życzymy błogosławionej
         Niedzieli.</p>
         <p>
-            <span style="float: left;">Sandomierz, 24.&nbsp;września 2023&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 15.&nbsp;października 2023&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
         </p>
     </div>
