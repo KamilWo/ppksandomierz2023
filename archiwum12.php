b223f5d1042288fca05ff9eb55b60c9d38c986c5
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2023-10-29 22:08:46 +0000

    2023-10-29

--- archiwum12.php
+++ archiwum12.php
@@ -19,7 +19,229 @@ include 'head.php';
                     <!-- treść główna -->
                     <div class="col">
                         <h2 class="first-content-header">Archiwum wiadomości 2023 rok</h2>
-                        <h2>Okres 1.&nbsp;stycznia - 22.&nbsp;października 2023&nbsp;r.</h2>
+                        <h2>Okres 1.&nbsp;stycznia - 29.&nbsp;października 2023&nbsp;r.</h2>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>INTENCJE MSZY ŚWIĘTYCH<br>
+                                    23.10. – 29.10.2023&nbsp;r.
+                                </h3>
+                            </div>
+                            <div class="col">
+                        <table class="aktualnosci_table" cellpadding="5" cellspacing="0">
+                            <tbody>
+                                <tr class="actualnosci_day">
+                                    <td><b>PONIEDZIAŁEK, 23&nbsp;października</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Pawła Guza</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Paulinę w&nbsp;rocz.&nbsp;śm., Mieczysława Ramusów,
+                                    Jarosława, Genowefę, Stefana Burków, Tadeusza Ramusa</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Barbarę Byra</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>WTOREK, 24&nbsp;października</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Pawła Guza</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Leonarda i&nbsp;Anielę Konopków</td></tr>
+                                    <tr style="border-bottom: 2px solid black;">
+                                    <td>+ Stefanię, Aleksandra Zimnickich, Władysławę,
+                                    Franciszka i&nbsp;Janusza Placha</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>ŚRODA, 25&nbsp;października</b></td>
+                                    <td><b>Wspomnienie św. Jana Pawła II, papieża</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Krystynę Mikula</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>W&nbsp;intencji wynagradzającej
+                                    PB&nbsp;za&nbsp;grzechy aborcji i&nbsp;eutanazji</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Stanisława 7&nbsp;rocz.&nbsp;śm. i&nbsp;Helenę
+                                    Boduła</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>CZWARTEK, 26&nbsp;października</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Pawła Guza</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Bolesława Grządkowskiego 4&nbsp;rocz.&nbsp;śm.</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Helenę i&nbsp;Stanisława Woźniaków</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>PIĄTEK, 27&nbsp;października</b></td>
+                                    <td><b></b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Krystynę Mikula</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Barbarę Byra</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Pawła Guza</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>SOBOTA, 28&nbsp;października</b></td>
+                                    <td><b>Święto Świętych Apostołów Szymona i&nbsp;Judy
+                                    Tadeusza</b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">6.30</td>
+                                    <td>+ Bogusława Tetera</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr>
+                                    <td class="center" rowspan="2">18.00</td>
+                                    <td>+ Wacława, Stanisława, Karolinę, Stanisława Kmicików</td>
+                                </tr><tr style="border-bottom: 2px solid black;">
+                                    <td>+ Alicję Machulę</td>
+                                </tr>
+                                <tr class="actualnosci_day">
+                                    <td><b>NIEDZIELA, 29&nbsp;października</b></td>
+                                    <td><b>
+                                        <span style="color: red">XXX Niedziela Zwykła</span>
+                                    </b></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">7.30</td>
+                                    <td>Za&nbsp;parafian</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">9.30</td>
+                                    <td>+ Jana Wnuka 22&nbsp;rocz.&nbsp;śm.</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">11.15</td>
+                                    <td>+ Mariusza 3&nbsp;rocz.&nbsp;śm., Tadeusza
+                                    2&nbsp;rocz.&nbsp;śm., Lipczyńskich; Wiesława Szala
+                                    i&nbsp;Macieja Ziemnickiego</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">12.30</td>
+                                    <td>+ Józefa, Władysławę, Władysława, Aleksandra, Marię,
+                                    Alicję Gospodarczyków i&nbsp;Grębów, Helenę, Mariannę
+                                    i&nbsp;Dymitra Chlebników</td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">17.30</td>
+                                    <td><strong>Nabożeństwo Różańcowe</strong></td>
+                                </tr>
+                                <tr style="border-bottom: 2px solid black;">
+                                    <td class="center">18.00</td>
+                                    <td>Dzięk-błag. o&nbsp;Miłosierdzie Boże, wiarę, nadzieję
+                                    i&nbsp;miłość, łaskę pokory, nawrócenie i&nbsp;uzdrowienie
+                                    w&nbsp;rodzinie</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="card">
+                            <div class="card-header">
+                                <h3>22.&nbsp;października 2023&nbsp;r.</h3>
+                                <h3>XXIX&nbsp;Niedziela Zwykła, Rok&nbsp;A</h3>
+                            </div>
+                            <div class="col">
+                                <p>1. W&nbsp;minioną niedzielę do&nbsp;puszek na&nbsp;wsparcie Dzieła
+                                Nowego Tysiąclecia – stypendia dla zdolnej a&nbsp;ubogiej młodzieży
+                                zebrane zostało 3758&nbsp;zł. <em>Bóg zapłać</em> wszystkim
+                                ofiarodawcom. Pieniądze zostały przekazane na&nbsp;wspomniany cel.</p>
+                                <p>2. Nabożeństwo różańcowe w&nbsp;październiku o&nbsp;godzinie 17.30
+                                w&nbsp;dni powszednie i&nbsp;w&nbsp;niedzielę. Zapraszamy
+                                do&nbsp;wzięcia udziału w&nbsp;tej pięknej modlitwie.</p>
+                                <p>3. Dziś w&nbsp;naszej parafii liczymy wiernych uczestniczących
+                                we&nbsp;Mszach&nbsp;św. (Dominicantes) i&nbsp;przyjmujących
+                                Komunie&nbsp;św. (Comunicantes). Obliczenia przekażemy w&nbsp;przyszłą
+                                niedzielę.</p>
+                                <p>4. Ta&nbsp;niedziela jest tzw.&nbsp;<strong>Niedzielą
+                                Misyjną</strong> i&nbsp;rozpoczyna <strong>Tydzień Misyjny</strong>,
+                                który ma&nbsp;na&nbsp;celu gorącą modlitwę i&nbsp;troskę
+                                o&nbsp;zrozumienie przez wiernych potrzeb misyjnych. Na&nbsp;różańcu
+                                będziemy się modlić w&nbsp;intencji misjonarzy, zwłaszcza
+                                za&nbsp;tych, pochodzących z&nbsp;naszej diecezji.</p>
+                                <p>5. W&nbsp;najbliższa sobotę o&nbsp;godz.&nbsp;18.00 w&nbsp;naszym
+                                parafialnym kościele odbędzie się kolejny wieczór Uwielbienia.
+                                Zapraszamy.</p>
+                                <p>6. W&nbsp;przyszłą niedzielę na&nbsp;Mszy Świętej
+                                o&nbsp;godz.&nbsp;12.30 rozstrzygnięcie konkursu plastycznego dla
+                                dzieci.</p>
+                                <p>7. Spotkanie dla kandydatów do&nbsp;bierzmowania z&nbsp;klas VI,
+                                VII i&nbsp;VIII we&nbsp;wtorek 7&nbsp;listopada po&nbsp;Mszy Świętej
+                                wieczorowej w&nbsp;kościele. Przypominamy, że&nbsp;z&nbsp;końcem
+                                października upływa termin złożenia deklaracji do&nbsp;bierzmowania.
+                                Kto jeszcze deklaracji nie złożył, prosimy aby to&nbsp;uczynił
+                                w&nbsp;najbliższym czasie.</p>
+                                <p>8. Zachęcamy wszystkich rodziców, rodziców chrzestnych, osoby,
+                                które straciły dziecko do&nbsp;włączenia się w&nbsp;Różaniec rodziców
+                                za&nbsp;dzieci. Kilka osób już się zgłosiło. Szczegółowe informacje
+                                u&nbsp;ks.&nbsp;Krystiana Musiała.</p>
+                                <p>9. Wszystkie dzieci zachęcamy do&nbsp;wzięcia udziału
+                                w&nbsp;korowodzie świętych, który w&nbsp;naszej parafii odbędzie się
+                                w&nbsp;niedziele 5&nbsp;listopada. Zachęcamy aby na&nbsp;ten dzień
+                                dzieci przyszły do&nbsp;kościoła przebrane za&nbsp;świętego.
+                                Szczegółowe informacje zostaną podane w&nbsp;przyszła niedzielę.</p>
+                                <p>10. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
+                                Zachęcamy do&nbsp;lektury prasy katolickiej.</p>
+                                <p>11. Dziękujemy za&nbsp;modlitwę w&nbsp;intencji kapłanów,
+                                za&nbsp;wszelką pomoc, złożone ofiary na&nbsp;rzecz parafii.
+                                Wszystkim uczestniczącym we&nbsp;Mszy&nbsp;św. życzymy błogosławionej
+                                niedzieli.</p>
+                                <p>
+                                    <span style="float: left;">Sandomierz, 22.&nbsp;października 2023&nbsp;r.</span>
+                                    <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
+                                </p>
+                            </div>
+                        </div>
+                        <br>
 
                         <div class="card">
                             <div class="card-header">
--- last_post.php
+++ last_post.php
@@ -1,55 +1,82 @@
 <div class="card">
     <div class="card-header">
-        <h3>22.&nbsp;października 2023&nbsp;r.</h3>
-        <h3>XXIX&nbsp;Niedziela Zwykła, Rok&nbsp;A</h3>
+        <h3>29.&nbsp;października 2023&nbsp;r.</h3>
+        <h3>XXX&nbsp;Niedziela Zwykła, Rok&nbsp;A</h3>
     </div>
     <div class="col">
-        <p>1. W&nbsp;minioną niedzielę do&nbsp;puszek na&nbsp;wsparcie Dzieła
-        Nowego Tysiąclecia – stypendia dla zdolnej a&nbsp;ubogiej młodzieży
-        zebrane zostało 3758&nbsp;zł. <em>Bóg zapłać</em> wszystkim
-        ofiarodawcom. Pieniądze zostały przekazane na&nbsp;wspomniany cel.</p>
-        <p>2. Nabożeństwo różańcowe w&nbsp;październiku o&nbsp;godzinie 17.30
-        w&nbsp;dni powszednie i&nbsp;w&nbsp;niedzielę. Zapraszamy
-        do&nbsp;wzięcia udziału w&nbsp;tej pięknej modlitwie.</p>
-        <p>3. Dziś w&nbsp;naszej parafii liczymy wiernych uczestniczących
-        we&nbsp;Mszach&nbsp;św. (Dominicantes) i&nbsp;przyjmujących
-        Komunie&nbsp;św. (Comunicantes). Obliczenia przekażemy w&nbsp;przyszłą
-        niedzielę.</p>
-        <p>4. Ta&nbsp;niedziela jest tzw.&nbsp;<strong>Niedzielą
-        Misyjną</strong> i&nbsp;rozpoczyna <strong>Tydzień Misyjny</strong>,
-        który ma&nbsp;na&nbsp;celu gorącą modlitwę i&nbsp;troskę
-        o&nbsp;zrozumienie przez wiernych potrzeb misyjnych. Na&nbsp;różańcu
-        będziemy się modlić w&nbsp;intencji misjonarzy, zwłaszcza
-        za&nbsp;tych, pochodzących z&nbsp;naszej diecezji.</p>
-        <p>5. W&nbsp;najbliższa sobotę o&nbsp;godz.&nbsp;18.00 w&nbsp;naszym
-        parafialnym kościele odbędzie się kolejny wieczór Uwielbienia.
-        Zapraszamy.</p>
-        <p>6. W&nbsp;przyszłą niedzielę na&nbsp;Mszy Świętej
-        o&nbsp;godz.&nbsp;12.30 rozstrzygnięcie konkursu plastycznego dla
-        dzieci.</p>
-        <p>7. Spotkanie dla kandydatów do&nbsp;bierzmowania z&nbsp;klas VI,
-        VII i&nbsp;VIII we&nbsp;wtorek 7&nbsp;listopada po&nbsp;Mszy Świętej
-        wieczorowej w&nbsp;kościele. Przypominamy, że&nbsp;z&nbsp;końcem
-        października upływa termin złożenia deklaracji do&nbsp;bierzmowania.
-        Kto jeszcze deklaracji nie złożył, prosimy aby to&nbsp;uczynił
-        w&nbsp;najbliższym czasie.</p>
-        <p>8. Zachęcamy wszystkich rodziców, rodziców chrzestnych, osoby,
-        które straciły dziecko do&nbsp;włączenia się w&nbsp;Różaniec rodziców
-        za&nbsp;dzieci. Kilka osób już się zgłosiło. Szczegółowe informacje
-        u&nbsp;ks.&nbsp;Krystiana Musiała.</p>
-        <p>9. Wszystkie dzieci zachęcamy do&nbsp;wzięcia udziału
+        <p>1. W&nbsp;zeszłą niedziele w&nbsp;naszej parafii liczyliśmy
+        wiernych uczestniczących we&nbsp;Mszach św.(Dominicantes)
+        i&nbsp;przyjmujących Komunie&nbsp;św.(Comunicantes). Takie
+        są&nbsp;dane: we&nbsp;Mszach św. uczestniczyło 931&nbsp;osób
+        (568&nbsp;kobiet i&nbsp;363&nbsp;mężczyzn), do&nbsp;Komunii św.
+        przystąpiło 468&nbsp;osób (317&nbsp;kobiet
+        i&nbsp;151&nbsp;mężczyzn).</p>
+        <p>2. W&nbsp;najbliższą środę - <strong>1&nbsp;listopada</strong> -
+        Uroczystość Wszystkich Świętych. W&nbsp;naszym parafialnym
+        kościele porządek Mszy&nbsp;św. jak w&nbsp;niedzielę, z&nbsp;tym
+        wyjątkiem, że&nbsp;Mszy&nbsp;św. o&nbsp;godz.&nbsp;12.30 nie będzie.
+        Na&nbsp;cmentarzu komunalnym Msza&nbsp;św. o&nbsp;godz.&nbsp;12.30,
+        na&nbsp;cmentarzu katedralnym o&nbsp;godz.&nbsp;13.00
+        i&nbsp;świętopawelskim Msza&nbsp;św. o&nbsp;godz.&nbsp;14.00.
+        Przypominamy o&nbsp;możliwości zyskania odpustu zupełnego
+        i&nbsp;ofiarowania go&nbsp;za&nbsp;zmarłych od&nbsp;1
+        do&nbsp;8&nbsp;listopada. Warunkiem uzyskania odpustu jest
+        nawiedzenie cmentarza lub kościoła w&nbsp;tym okresie, oraz
+        wypełnienie zwykłych warunków, tj.&nbsp;przystąpienie do&nbsp;komunii
+        św. w&nbsp;dniu nawiedzenia cmentarza lub kościoła i&nbsp;odmówienie
+        modlitw "Ojcze nasz" i&nbsp;"Wierzę w&nbsp;Boga", i&nbsp;jakiejkolwiek
+        modlitwy w&nbsp;intencjach wyznaczonych na&nbsp;dany dzień przez
+        papieża.</p>
+        <p>3. W&nbsp;dzień zaduszny 2&nbsp;listopada w&nbsp;naszym kościele
+        będą sprawowane Msze&nbsp;św. o&nbsp;godz.&nbsp;6.30, następna
+        za&nbsp;zmarłych polecanych w&nbsp;wypominkach z&nbsp;procesją
+        po&nbsp;kościele i&nbsp;modlitwami za&nbsp;zmarłych
+        o&nbsp;godz.&nbsp;9.00. O&nbsp;godz.&nbsp;17.30. Koronka
+        do&nbsp;Bożego Miłosierdzia połączona z&nbsp;wypominkami
+        i&nbsp;Msza&nbsp;św. o&nbsp;godz.&nbsp;18.00.</p>
+        <p>4. W&nbsp;dniach od&nbsp;1 do&nbsp;8&nbsp;listopada
+        godz.&nbsp;17.30 Koronka do&nbsp;Bożego Miłosierdzia
+        z&nbsp;wypominkami za&nbsp;zmarłych.</p>
+        <p>5. Rocznica śmierci Sługi Bożego Biskupa Piotra Gołębiowskiego
+        przypada 2&nbsp;listopada – módlmy się o&nbsp;rychłą beatyfikację.</p>
+        <p>6. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka dla
+        dorosłych, młodzieży i&nbsp;dla dzieci. Zachęcamy do&nbsp;lektury
+        prasy katolickiej.</p>
+        <p>7. W&nbsp;tym tygodniu Pierwszy czwartek, piątek, sobota miesiąca.
+        W&nbsp;Pierwszy Piątek Miesiąca, spowiedź w&nbsp;tym dniu
+        od&nbsp;godz.&nbsp;17.30.</p>
+        <p>8. W&nbsp;najbliższą sobotę o&nbsp;godz.&nbsp;17.15. – nabożeństwo
+        Różańcowe poprowadzi Legion Maryi z&nbsp;naszej parafii.</p>
+        <p>9. W&nbsp;przyszłą niedzielę w&nbsp;kościele, pół godziny przed
+        Mszą&nbsp;św., która sprawowana jest o&nbsp;godz.&nbsp;11.15 –
+        adoracja Najśw. Sakramentu połączona z&nbsp;modlitwą różańcową, którą
+        poprowadzi Koło Żywego Różańca z&nbsp;naszej parafii i&nbsp;zmiana
+        tajemnic różańcowych. Zapraszamy do&nbsp;wspólnej modlitwy.</p>
+        <p>10. Spotkanie dla kandydatów do&nbsp;bierzmowania
+        z&nbsp;klas&nbsp;VI, VII i&nbsp;VIII we&nbsp;wtorek 7&nbsp;listopada
+        po&nbsp;Mszy Świętej wieczorowej w&nbsp;kościele. Przypominamy,
+        że&nbsp;z&nbsp;końcem października upływa termin złożenia deklaracji
+        do&nbsp;bierzmowania. Kto jeszcze deklaracji nie złożył, prosimy aby
+        to&nbsp;uczynił w&nbsp;najbliższym czasie. Najbliższe spotkanie
+        poprowadzi młodzież z&nbsp;Katolickiego Stowarzyszenia Młodzieży
+        z&nbsp;diecezji.</p>
+        <p>11. Wszystkich chłopców, którzy chcieliby zostać ministrantami
+        i&nbsp;służyć przy ołtarzu oraz dziewczynki, które chciałby śpiewać
+        w&nbsp;scholi, prosimy o&nbsp;kontakt z&nbsp;księżmi wikariuszami.
+        Zwracamy się także do&nbsp;rodziców z&nbsp;prośbą, by&nbsp;zachęcali
+        i&nbsp;umożliwili dzieciom uczęszczanie na&nbsp;spotkaniach
+        organizowane przy parafii. Za&nbsp;wszelką pomoc – dziękujemy.</p>
+        <p>12. Wszystkie dzieci zachęcamy do&nbsp;wzięcia udziału
         w&nbsp;korowodzie świętych, który w&nbsp;naszej parafii odbędzie się
-        w&nbsp;niedziele 5&nbsp;listopada. Zachęcamy aby na&nbsp;ten dzień
+        w&nbsp;niedziele 5&nbsp;listopada. Zachęcamy, aby na&nbsp;ten dzień
         dzieci przyszły do&nbsp;kościoła przebrane za&nbsp;świętego.
-        Szczegółowe informacje zostaną podane w&nbsp;przyszła niedzielę.</p>
-        <p>10. Na&nbsp;stoliku pod chórem jest najnowsza prasa katolicka.
-        Zachęcamy do&nbsp;lektury prasy katolickiej.</p>
-        <p>11. Dziękujemy za&nbsp;modlitwę w&nbsp;intencji kapłanów,
-        za&nbsp;wszelką pomoc, złożone ofiary na&nbsp;rzecz parafii.
-        Wszystkim uczestniczącym we&nbsp;Mszy&nbsp;św. życzymy błogosławionej
-        niedzieli.</p>
+        Rozpoczęcie korowodu o&nbsp;godz.&nbsp;12.15 przy figurze Matki Bożej
+        przed kościołem i&nbsp;następnie Msza Świętą
+        o&nbsp;godz.&nbsp;12.30.</p>
+        <p>13. Wszystkim uczestniczącym we&nbsp;Mszy&nbsp;św. życzymy
+        błogosławionej niedzieli.</p>
         <p>
-            <span style="float: left;">Sandomierz, 22.&nbsp;października 2023&nbsp;r.</span>
+            <span style="float: left;">Sandomierz, 29.&nbsp;października 2023&nbsp;r.</span>
             <span style="float: right; text-align: right;">ks.&nbsp;proboszcz Stanisław Chmielewski</span>
         </p>
     </div>
